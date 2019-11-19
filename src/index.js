1098846c3d00b3494f779c2160a1758cc7e16021
Jorge Vera <me@jorgevera.net>
2019-11-19 00:04:28 -0500

    Create a client
    docs/react/v3.0-beta/get-started/#create-a-client

--- src/index.js
+++ src/index.js
@@ -1,10 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { ApolloClient, HttpLink, gql, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: "https://api.github.com/graphql",
+    headers: {
+      Authorization: localStorage.getItem("token")
+    }
+  }),
+  cache: new InMemoryCache({
+    typePolicies: {
+      User: {
+        keyFields: ["login"]
+      }
+    }
+  })
+});
+
+const GET_USER = gql`
+  {
+    user(login: "jorgeveranet") {
+      login
+      name
+      bio
+      avatarUrl(size: 75)
+      url
+    }
+  }
+`;
+
+client
+  .query({
+    query: GET_USER
+  })
+  .then(result => console.log(result))
+  .catch(err => console.log(err));
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
